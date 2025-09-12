d3f355f00341e62738b0635ee3535960d8d441d6
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2025-09-11 22:45:41 -0400

    feat: Mostrar penalidad si se perdio ticket.

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import { calcularEstadiaDiurnaYNocturna, calcularTarifaTotal, aplicarTope } from "./tarifas.js";
+import { calcularEstadiaDiurnaYNocturna, calcularTarifaTotal, aplicarTope, verificarEstadoTicket } from "./tarifas.js";
 
 const in_ingreso = document.querySelector("#ingreso");
 const in_salida = document.querySelector("#salida");
@@ -9,13 +9,6 @@ const radios = document.getElementsByName("estadoTicket");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const hora_i = in_ingreso.value;
-  const hora_s = in_salida.value;
-
-  const [horasdia, minutosdia, horasnoct, minutosnoct] = calcularEstadiaDiurnaYNocturna(hora_i, hora_s);
-  const tarifaTotal = calcularTarifaTotal(horasdia, minutosdia, horasnoct, minutosnoct);
-  const topeAplicado = aplicarTope(tarifaTotal);
-
   let seleccionado = null;
   for (const r of radios) {
     if (r.checked) {
@@ -24,5 +17,24 @@ form.addEventListener("submit", (event) => {
     }
   }
 
-  div.innerHTML = "<p>Horas día:" + horasdia + ", minutos día: " + minutosdia + ", horas noche: " + horasnoct + ", minutos noche: " + minutosnoct + "</p><br><p>Tarifa total con tope aplicado: " + topeAplicado + " Bs.</p><br></p>Estado del ticket: " + seleccionado + "</p>";
+  let tarifaTotal = 0;
+
+  let estadoTicket = verificarEstadoTicket(seleccionado);
+
+  if (estadoTicket === "80,00") {
+    tarifaTotal = estadoTicket;
+
+    div.innerHTML = "<p>Penalidad por ticket perdido: " + tarifaTotal + " Bs.</p>";
+  } else {
+    const hora_i = in_ingreso.value;
+    const hora_s = in_salida.value;
+
+    const [horasdia, minutosdia, horasnoct, minutosnoct] = calcularEstadiaDiurnaYNocturna(hora_i, hora_s);
+    tarifaTotal = calcularTarifaTotal(horasdia, minutosdia, horasnoct, minutosnoct);
+    const topeAplicado = aplicarTope(tarifaTotal);
+
+    div.innerHTML = "<p>Horas día:" + horasdia + ", minutos día: " + minutosdia + ", horas noche: " + horasnoct + ", minutos noche: " + minutosnoct + "</p><br><p>Tarifa total con tope aplicado: " + topeAplicado + " Bs.</p><br></p>Estado del ticket: " + seleccionado + "</p>";
+  }
+
+  //div.innerHTML = "<p>Horas día:" + horasdia + ", minutos día: " + minutosdia + ", horas noche: " + horasnoct + ", minutos noche: " + minutosnoct + "</p><br><p>Tarifa total con tope aplicado: " + topeAplicado + " Bs.</p><br></p>Estado del ticket: " + seleccionado + "</p>";
 });
